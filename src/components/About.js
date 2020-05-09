af41b99d4a8edfdbbff5095ff0fa2de706dac47e
mohdraqif <mohdraqif94@gmail.com>
2020-05-10 02:11:21 +0530

    About Page Completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about" id="about">
-      Hello from About
+      <div className="about-info">
+        <div className="about-content">
+          <h1>We are LA 16</h1>
+          <h4>Serving since 2010, we are as proud as thankful for all the support. Our team at LA 16 is committed to serving you the best you ever had. </h4>
+          <h4>Your taste is our priority. A well-expertised team at our hand we serve the best food of the county and will continue to do so.</h4>
+          <h4>A 3-time Award-winning place is waiting for you. We love giving a reason for happiness to our people. Come again soon.</h4>
+        </div>
+      </div>
     </div>
   );
 }
