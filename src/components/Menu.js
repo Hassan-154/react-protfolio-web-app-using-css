b69851b902658fd5b3c5335706c7b16c8d6de5c6
mohdraqif <mohdraqif94@gmail.com>
2020-05-13 02:29:01 +0530

    Menu Page Started

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,15 +7,78 @@ const Menu = () => {
         <h1>Welcome to our diverse menu table</h1>
       </div>
       <div className="grid">
-        <div className="project1 projects--medium"></div>
-        <div className="project2 projects--medium"></div>
-        <div className="project3 projects--medium"></div>
-        <div className="project4 projects--medium"></div>
-        <div className="project5 projects--medium"></div>
-        <div className="project6 projects--medium"></div>
-        <div className="project7 projects--medium"></div>
-        <div className="project8 projects--medium"></div>
-        <div className="project9 projects--medium"></div>
+        <div className="project1 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project2 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project3 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project4 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project5 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project6 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project7 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project8 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
+        <div className="project9 projects--medium">
+          <div className="menu-card">
+            <h1>Dish Name</h1>
+            <div className="menu-name">
+              <p>Free Y/N</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
