b6e6e211e5fee554295910a93d499949b1ebda92
Hassan <hassanbajwa154@gmail.com>
2022-06-11 21:33:26 +0500

    Updates

---
+++ precache-manifest.d8f62c3d46f3ae83939e11a78fd9c0da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87303fd429a86b10bafcd988494c4551",
+    "url": "/react-protfolio-web-app-using-css/index.html"
+  },
+  {
+    "revision": "d6232f404673cc87fc78",
+    "url": "/react-protfolio-web-app-using-css/static/css/main.fbdff0bd.chunk.css"
+  },
+  {
+    "revision": "233f0ee43d36b71ee730",
+    "url": "/react-protfolio-web-app-using-css/static/js/2.35d3ad84.chunk.js"
+  },
+  {
+    "revision": "4e0ccdbfb8d8e4f5e8739c71e90f05ac",
+    "url": "/react-protfolio-web-app-using-css/static/js/2.35d3ad84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6232f404673cc87fc78",
+    "url": "/react-protfolio-web-app-using-css/static/js/main.15c06e4b.chunk.js"
+  },
+  {
+    "revision": "ad13f26257f2b0a1d272",
+    "url": "/react-protfolio-web-app-using-css/static/js/runtime-main.cf14f50e.js"
+  },
+  {
+    "revision": "67043764a7dfe3831eb20cab34ea00d3",
+    "url": "/react-protfolio-web-app-using-css/static/media/jefferson-santos-9SoCnyQmkzI-unsplash.67043764.jpg"
+  },
+  {
+    "revision": "33aa9322718a4508a9d684246938fc3e",
+    "url": "/react-protfolio-web-app-using-css/static/media/xd.33aa9322.png"
+  }
+]);
